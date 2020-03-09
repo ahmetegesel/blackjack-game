b2bce43fee45b7a04408ba9cbf1a70daad0353e2
Ahmet Egesel <ahmet.egesel@albil.com.tr>
2020-03-09 20:42:57 +0100

    Action class is documented.

--- src/action.js
+++ src/action.js
@@ -1,4 +1,14 @@
+/**
+ * Base class for actions.
+ *
+ * You do not hold the instance of this class and invoke this function to dispatch actions.
+ * You should only pass the instance of this class to registerAction function of
+ * the StoreNode instance.
+ * */
 export class Action {
+  /**
+   * This is function is invoked by the StoreNode instance.
+   * */
   dispatch(action, payload, context) {
     throw new Error('Not Implemented Error');
   }
