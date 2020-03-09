edf443350a9d775016ca33c8cce0b30eb157f413
Ahmet Egesel <ahmet.egesel@albil.com.tr>
2020-03-09 20:54:09 +0100

    SplitAction is documented with TODOs.

--- src/player/splitAction.js
+++ src/player/splitAction.js
@@ -1,5 +1,16 @@
 import { Action } from "../action";
 
+/*
+* TODO implement this class in a way that it can manipulate the current actions
+*  of the owning StoreNode instance. You should replace current hit and stand
+*  actions with new ones. These new actions should handle split hands using
+*  the state of the Player StoreNode. These new actions should pass
+*  action parameter of the dispatch of the class so that you can know
+*  action to be handled. If it is a hit then you should use normal behaviour
+*  of the hit to draw card but store it in split hands in the state. If stand
+*  is dispatched, then it should behave like normal stand but additionally
+*  it should restore default actions of the StoreNode.
+* */
 export class SplitAction extends Action {
 
 }
