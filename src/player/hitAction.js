4ef39b0f214b11f1f0dec4d956ece9bffe487691
Ahmet Egesel <ahmet.egesel@albil.com.tr>
2020-03-09 23:43:26 +0100

    Documented hitAction.js

--- src/player/hitAction.js
+++ src/player/hitAction.js
@@ -5,7 +5,7 @@ import { drawFromDeck } from "../helpers/deck";
 export class HitAction extends Action {
   dispatch(action, payload, state) {
     const { deck, deckIndices, pick } = payload;
-    let aceCount = state.aceCount || 0;
+    let aceCount = state.aceCount || 0; // this will then be used to calculate the score.
 
     const drawnCard = drawFromDeck(deck, deckIndices, pick);
     const cards = [...state.cards, drawnCard];
